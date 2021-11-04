cd6a520b10fac3f09c130435abaf2882e715fcae
agustinrodriguezdeirossi <slay1991@hotmail.com>
2021-11-04 19:38:44 -0300

    changes

--- src/app/components/character-list/character-list.component.ts
+++ src/app/components/character-list/character-list.component.ts
@@ -1,37 +1,38 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {CharactersService} from "../../services/characters.service";
-import {Character} from "../../models/characters";
-import {map, take} from "rxjs/operators";
-import {PageEvent} from "@angular/material/paginator";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { CharactersService } from '../../services/characters.service';
+import { Character } from '../../models/characters';
+import { map, take } from 'rxjs/operators';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-character-list',
   templateUrl: './character-list.component.html',
-  styleUrls: ['./character-list.component.scss']
+  styleUrls: ['./character-list.component.scss'],
 })
 export class CharacterListComponent implements OnInit, OnDestroy {
-
-  characters: Character[] = []
+  characters: Character[] = [];
   lenght: number = 0;
   pageSize: number = 20;
 
-  constructor(private charactersService: CharactersService) {
-  }
+  constructor(private charactersService: CharactersService) {}
 
   ngOnInit(): void {
-    this.charactersService.getAll().pipe(
-      take(1),
-      map(({data}) => {
-        this.lenght = data.characters.info.count;
-        return data.characters;
-      }),
-      map(({results}) => {
-        this.characters = results;
-      })
-    ).subscribe();
-    this.charactersService.lookedUpCharacter.subscribe(() =>{
+    this.charactersService
+      .getAll()
+      .pipe(
+        take(1),
+        map(({ data }) => {
+          this.lenght = data.characters.info.count;
+          return data.characters;
+        }),
+        map(({ results }) => {
+          this.characters = results;
+        })
+      )
+      .subscribe();
+    this.charactersService.lookedUpCharacter.subscribe(() => {
       this.getCharacter();
-    })
+    });
   }
 
   ngOnDestroy() {
@@ -39,54 +40,68 @@ export class CharacterListComponent implements OnInit, OnDestroy {
   }
 
   pageSelection(pageNumber: PageEvent) {
-    if(this.charactersService.lookedUpCharacter.getValue() != '') {
-      this.charactersService.searchCharacter(pageNumber.pageIndex == 0 ? 1 : pageNumber.pageIndex+1).pipe(
-        take(1),
-        map(({data}) => {
-          this.lenght = data.characters.info.count;
-          return data.characters;
-        }),
-        map(({results}) => {
-          this.characters = results;
-        })
-      ).subscribe();
+    if (this.charactersService.lookedUpCharacter.getValue() != '') {
+      this.charactersService
+        .searchCharacter(
+          pageNumber.pageIndex == 0 ? 1 : pageNumber.pageIndex + 1
+        )
+        .pipe(
+          take(1),
+          map(({ data }) => {
+            this.lenght = data.characters.info.count;
+            return data.characters;
+          }),
+          map(({ results }) => {
+            this.characters = results;
+          })
+        )
+        .subscribe();
     } else {
-      this.charactersService.getAll(pageNumber.pageIndex == 0 ? 1 : pageNumber.pageIndex+1).pipe(
-        take(1),
-        map(({data}) => {
-          this.lenght = data.characters.info.count;
-          return data.characters;
-        }),
-        map(({results}) => {
-          this.characters = results
-        })
-      ).subscribe();
+      this.charactersService
+        .getAll(pageNumber.pageIndex == 0 ? 1 : pageNumber.pageIndex + 1)
+        .pipe(
+          take(1),
+          map(({ data }) => {
+            this.lenght = data.characters.info.count;
+            return data.characters;
+          }),
+          map(({ results }) => {
+            this.characters = results;
+          })
+        )
+        .subscribe();
     }
   }
 
   getCharacter() {
-    if(this.charactersService.lookedUpCharacter.getValue() == '') {
-      this.charactersService.getAll().pipe(
-        take(1),
-        map(({data}) => {
-          this.lenght = data.characters.info.count;
-          return data.characters;
-        }),
-        map(({results}) => {
-          this.characters = results
-        })
-      ).subscribe();
+    if (this.charactersService.lookedUpCharacter.getValue() == '') {
+      this.charactersService
+        .getAll()
+        .pipe(
+          take(1),
+          map(({ data }) => {
+            this.lenght = data.characters.info.count;
+            return data.characters;
+          }),
+          map(({ results }) => {
+            this.characters = results;
+          })
+        )
+        .subscribe();
     } else {
-      this.charactersService.searchCharacter().pipe(
-        take(1),
-        map(({data}) => {
-          this.lenght = data.characters.info.count;
-          return data.characters;
-        }),
-        map(({results}) => {
-          this.characters = results;
-        })
-      ).subscribe();
+      this.charactersService
+        .searchCharacter()
+        .pipe(
+          take(1),
+          map(({ data }) => {
+            this.lenght = data.characters.info.count;
+            return data.characters;
+          }),
+          map(({ results }) => {
+            this.characters = results;
+          })
+        )
+        .subscribe();
     }
   }
 }
--- src/app/components/character/character.component.ts
+++ src/app/components/character/character.component.ts
@@ -1,28 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import {CharactersService} from "../../services/characters.service";
-import {map, take, tap} from "rxjs/operators";
-import {Character} from "../../models/characters";
-import {ActivatedRoute} from "@angular/router";
+import { CharactersService } from '../../services/characters.service';
+import { map, take, tap } from 'rxjs/operators';
+import { Character } from '../../models/characters';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-character',
   templateUrl: './character.component.html',
-  styleUrls: ['./character.component.scss']
+  styleUrls: ['./character.component.scss'],
 })
 export class CharacterComponent implements OnInit {
+  character!: Character;
 
-  character!: Character
-
-  constructor(private charactersService: CharactersService, private activatedRoute: ActivatedRoute) { }
+  constructor(
+    private charactersService: CharactersService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.charactersService.getOne(this.activatedRoute.snapshot.params.id).pipe(
-      take(1),
-      map(({data}) => data.character),
-      tap(character => {
-        this.character = character;
-      })
-      ).subscribe();
+    this.charactersService
+      .getOne(this.activatedRoute.snapshot.params.id)
+      .pipe(
+        take(1),
+        map(({ data }) => data.character),
+        tap((character) => {
+          this.character = character;
+        })
+      )
+      .subscribe();
   }
-
 }
